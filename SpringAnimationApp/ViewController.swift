800367f7286d30a3e4e1acf9c3b57f3c9ab69aa4
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2022-09-02 13:27:24 +0300

    Initial Commit

---
+++ SpringAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationApp
+//
+//  Created by Lera Savchenko on 2.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
