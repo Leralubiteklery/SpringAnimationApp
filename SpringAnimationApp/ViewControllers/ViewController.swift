7d253994b3f34c2b3c675a55f63477601c2d3b60
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-06-18 12:58:10 +0300

    Implemented ViewController

--- SpringAnimationApp/ViewControllers/ViewController.swift
+++ SpringAnimationApp/ViewControllers/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var animationView: SpringView!
+    @IBOutlet weak var animationLabel: UILabel! {
+        didSet {
+            animationLabel.text = animation.description
+        }
     }
-
-
+    
+    private var animation = Animation.getAnimation()
+    
+    
+    @IBAction func animationButtonPressed(_ sender: UIButton) {
+        animationLabel.text = animation.description
+        
+        animationView.animation = animation.name
+        animationView.curve = animation.curve
+        animationView.force = animation.force
+        animationView.duration = animation.duration
+        animationView.delay = animation.delay
+        animationView.animate()
+        
+        animation = Animation.getAnimation()
+        sender.setTitle("Run \(animation.name)", for: .normal)
+    }
 }
 
